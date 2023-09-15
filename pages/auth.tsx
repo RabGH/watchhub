843f966f81a698d6f58f63167419f1603509e1b2
RabGh <rabiighais@gmail.com>
2023-09-15 14:53:50 +0300

    Input auth page temp

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import Input from "@/components/input";
+
 const Auth = () => {
   return (
     <div className="homeImage relative h-full w-full bg-no-repeat bg-center bg-fixed bg-cover">
@@ -8,7 +10,12 @@ const Auth = () => {
           <Image width={200} height={48} src="/images/logo.png" alt="Logo" />
         </nav>
         <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full"></div>
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+            <div className="flex flex-col ga-4">
+              <Input />
+            </div>
+          </div>
         </div>
       </div>
     </div>
